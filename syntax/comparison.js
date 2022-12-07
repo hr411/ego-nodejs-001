7ddb3d87c818f73f64575c1ee70f199bf3e67807
hrsong <hrosng411@gmail.com>
2022-12-08 00:00:28 +0900

    init commit

---
+++ syntax/comparison.js
@@ -0,0 +1,13 @@
+const { log } = require("console");
+
+console.log(1==1); //true
+
+console.log(1==2); //false
+
+console.log(1>2); //false
+
+console.log(1<2); //tuue
+
+console.log(1===1); //ture
+
+console.log(1===2); //ture
